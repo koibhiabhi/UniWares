4ce523f34d93166760ec9c9c3452cacc1cd828b5
Abhinav Jain <avinavrashujain@gmail.com>
2024-02-03 18:51:53 +0530

    changed the app icon and login activity a bit.

--- app/src/main/java/com/example/uniwares/experiment.java
+++ app/src/main/java/com/example/uniwares/experiment.java
@@ -5,7 +5,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.view.ViewGroup;
-
 import com.airbnb.lottie.LottieAnimationView;
 
 public class experiment extends AppCompatActivity {
@@ -15,21 +14,5 @@ public class experiment extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_experiment);
 
-
-        LottieAnimationView animationView = findViewById(R.id.animationView);
-        DisplayMetrics displayMetrics = new DisplayMetrics();
-        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
-
-        int screenWidth = displayMetrics.widthPixels;
-        int screenHeight = displayMetrics.heightPixels;
-
-        // Adjust the width and height of the LottieAnimationView based on screen dimensions
-        int newWidth = (int) (screenWidth * 0.8); // Adjust as needed
-        int newHeight = (int) (screenHeight * 0.2); // Adjust as needed
-
-        ViewGroup.LayoutParams layoutParams = animationView.getLayoutParams();
-        layoutParams.width = newWidth;
-        layoutParams.height = newHeight;
-        animationView.setLayoutParams(layoutParams);
     }
 }
